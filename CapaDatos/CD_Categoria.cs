aadd56a105406317e230541a92ee99a424f8153a
JJMDT <127325507+JJMDT@users.noreply.github.com>
2024-06-17 22:48:48 -0300

    CRUD de categorias OK

---
+++ CapaDatos/CD_Categoria.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaEntidad;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace CapaDatos
+{
+    public class CD_Categoria
+    {
+        public List<Categoria> Listar()
+        {
+            List<Categoria> lista = new List<Categoria>();
+
+            try
+            {
+                using (SqlConnection oConn = new SqlConnection(Conexion.conn))
+                {
+                    string query = "select idCategoria,descripcion,activo from categoria";
+
+                    SqlCommand cmd = new SqlCommand(query, oConn);
+                    cmd.CommandType = CommandType.Text;
+
+                    oConn.Open();
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            lista.Add(
+                                new Categoria()
+                                {
+                                    idCategoria = Convert.ToInt32(dr["idCategoria"]),
+                                    descripcion = dr["descripcion"].ToString(),
+                                    activo = Convert.ToBoolean(dr["activo"])
+                                }
+                            );
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                lista = new List<Categoria>();
+            }
+
+            return lista;
+        }
+
+        public int Registrar(Categoria obj, out string mensaje)
+        {
+            int idAutogenerado = 0;
+            mensaje = string.Empty;
+            try
+            {
+                using (SqlConnection oConn = new SqlConnection(Conexion.conn))
+                {
+                    SqlCommand cmd = new SqlCommand("Registrar_Categoria", oConn);
+                    cmd.Parameters.AddWithValue("descripcion", obj.descripcion);
+                    cmd.Parameters.AddWithValue("activo", obj.activo);
+                    cmd.Parameters.Add("resultado", SqlDbType.Int).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("mensaje", SqlDbType.VarChar, 255).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    oConn.Open();
+                    cmd.ExecuteNonQuery();
+
+                    idAutogenerado = Convert.ToInt32(cmd.Parameters["resultado"].Value);
+                    mensaje = cmd.Parameters["mensaje"].Value.ToString();
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                idAutogenerado = 0;
+                mensaje = ex.Message;
+
+            }
+            return idAutogenerado;
+        }
+
+        public bool Editar(Categoria obj, out string mensaje)
+        {
+            bool resultado = false;
+            mensaje = string.Empty;
+            try
+            {
+                using (SqlConnection oConn = new SqlConnection(Conexion.conn))
+                {
+                    SqlCommand cmd = new SqlCommand("Editar_Categoria", oConn);
+
+                    cmd.Parameters.AddWithValue("idCategoria", obj.idCategoria);
+                    cmd.Parameters.AddWithValue("descripcion", obj.descripcion);
+                    cmd.Parameters.AddWithValue("activo", obj.activo);
+                    cmd.Parameters.Add("resultado", SqlDbType.Bit).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("mensaje", SqlDbType.VarChar, 255).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    oConn.Open();
+                    cmd.ExecuteNonQuery();
+
+                    resultado = Convert.ToBoolean(cmd.Parameters["resultado"].Value);
+                    mensaje = cmd.Parameters["mensaje"].Value.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                resultado = false;
+                mensaje = ex.Message;
+            }
+            return resultado;
+        }
+
+        public bool Eliminar(int id, out string mensaje)
+        {
+            bool resultado = false;
+            mensaje = string.Empty;
+            try
+            {
+                using (SqlConnection oConn = new SqlConnection(Conexion.conn))
+                {
+                    SqlCommand cmd = new SqlCommand("Eliminar_Categoria", oConn);
+
+                    cmd.Parameters.AddWithValue("idCategoria", id);
+                    cmd.Parameters.Add("resultado", SqlDbType.Bit).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("mensaje", SqlDbType.VarChar, 255).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    oConn.Open();
+                    cmd.ExecuteNonQuery();
+
+                    resultado = Convert.ToBoolean(cmd.Parameters["resultado"].Value);
+                    mensaje = cmd.Parameters["mensaje"].Value.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                resultado = false;
+                mensaje = ex.Message;
+            }
+            return resultado;
+        }
+
+       
+    }
+}
---
+++ CapaNegocio/CN_Categoria.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaDatos;
+using CapaEntidad;
+
+namespace CapaNegocio
+{
+    public class CN_Categoria
+    {
+        private CD_Categoria objCapaDato = new CD_Categoria();
+
+        public List<Categoria> Listar()
+        {
+            return objCapaDato.Listar();
+        }
+        // registrar 
+        public int Registrar(Categoria obj, out string mensaje)
+        {
+            mensaje = string.Empty;
+
+            if (string.IsNullOrEmpty(obj.descripcion) || string.IsNullOrWhiteSpace(obj.descripcion))
+            {
+                mensaje = "La descripcion de la catetegoia no puede ser vacio";
+            }
+           
+            // Proceder con el registro del usuario
+            if (string.IsNullOrEmpty(mensaje))
+            {
+                return objCapaDato.Registrar(obj, out mensaje);
+
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        //editar 
+
+        public bool Editar(Categoria obj, out string mensaje)
+        {
+            mensaje = string.Empty;
+
+            if (string.IsNullOrEmpty(obj.descripcion) || string.IsNullOrWhiteSpace(obj.descripcion))
+            {
+                mensaje = "La descripcion de la catetegoia no puede ser vacio";
+            }
+
+
+            // Proceder con el registro del usuario
+            if (string.IsNullOrEmpty(mensaje))
+            {
+                return objCapaDato.Editar(obj, out mensaje);
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        //eliminar 
+
+        public bool Eliminar(int id, out string mensaje)
+        {
+            return objCapaDato.Eliminar(id, out mensaje);
+        }
+    }
+}
